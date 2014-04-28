611586b1af4e9b876e28c573c084e58769157af1
aronkatona <katonaron@gmail.com>
2014-04-29 00:28:19 +0200

    Gui and shooting

--- src/network/Client.java
+++ src/network/Client.java
@@ -1,5 +1,6 @@
 package network;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
@@ -8,81 +9,118 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
 
 
 public class Client {
+	
+	private static final int PORT = 9006;
 
     BufferedReader in;
     PrintWriter out;
-    JFrame frame = new JFrame("Chatter");
-    JTextField textField = new JTextField(40);
-    JTextArea messageArea = new JTextArea(8, 40);
+    ClientGui gui ;
+    
+    Integer[][] table ;
+    Integer N;
+    
 
     public Client() {
 
-      
-        textField.setEditable(false);
-        messageArea.setEditable(false);
-        frame.getContentPane().add(textField, "North");
-        frame.getContentPane().add(new JScrollPane(messageArea), "Center");
-        frame.pack();
+        gui = new ClientGui(); 
 
-     
-        textField.addActionListener(new ActionListener() {
+        gui.textField.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                out.println(textField.getText());
-                textField.setText("");
+                out.println("CHAT" + gui.textField.getText());
+                gui.textField.setText("");
             }
         });
-    }
 
-    private String getServerAddress() {
-        return JOptionPane.showInputDialog(
-            frame,
-            "Enter IP Address of the Server:",
-            "Welcome to the Chatter",
-            JOptionPane.QUESTION_MESSAGE);
+        for(final JButton btn: gui.buttons){
+        	btn.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+						System.out.println(btn.getText());
+						out.println("FIRE" + btn.getText());
+					
+				}
+			});
+        }
     }
-
-    private String getName() {
-        return JOptionPane.showInputDialog(
-            frame,
-            "Choose a screen name:",
-            "Screen name selection",
-            JOptionPane.PLAIN_MESSAGE);
+    
+    public void sendMsg(){
+    	
     }
 
 
     private void run() throws IOException {
 
-        String serverAddress = getServerAddress();
-        Socket socket = new Socket(serverAddress, 9001);
-        in = new BufferedReader(new InputStreamReader(
-            socket.getInputStream()));
+        String serverAddress = "localhost";
+        Socket socket = new Socket(serverAddress, PORT);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         out = new PrintWriter(socket.getOutputStream(), true);
+        
 
         while (true) {
             String line = in.readLine();
-            if (line.startsWith("SUBMITNAME")) {
-                out.println(getName());
+             if(line.startsWith("SENDID")){
+            	 gui.setTitle("Client 0" + line.substring(6) );
+            } else if (line.startsWith("SUBMITNAME")) {
+                out.println(gui.getName());
             } else if (line.startsWith("NAMEACCEPTED")) {
-                textField.setEditable(true);
+                gui.textField.setEditable(true);
+            } else if (line.startsWith("INIT")){
+            	String tmp = line.substring(4, 6);
+            	N = Integer.parseInt(tmp);
+            	table = new Integer[N][N];
+            } else if(line.startsWith("FILL")){
+            	Integer i = Integer.parseInt(line.substring(4,5));
+            	Integer j = Integer.parseInt(line.substring(5,6));
+            	Integer value = Integer.parseInt(line.substring(6,7));
+            	table[i][j] = value;
+            } else if(line.startsWith("TABLEDONE")){
+            	 for(int i = 0; i < N; ++i){
+                 	for(int j = 0; j < N; ++j){
+                 		if(table[i][j] == 0){
+                 			String tmp = " " + Integer.toString(i) + "," + Integer.toString(j); 
+                 			for(JButton btn: gui.buttons){
+                 				if(btn.getText().equals(tmp)){
+                 					btn.setBackground(Color.WHITE);
+                 					break;
+                 				} 
+                 			}
+                 		} 
+                 		if(table[i][j] == 1){
+                 			String tmp = " " + Integer.toString(i) + "," + Integer.toString(j); 
+                 			for(JButton btn: gui.buttons){
+                 				if(btn.getText().equals(tmp)){
+                 					btn.setBackground(Color.RED);
+                 					break;
+                 				} 
+                 			}
+                 		} 
+                 	}
+                 }
+            	/*
+            	for(int i = 0; i < N; ++i){
+            		for(int j = 0; j < N; ++j){
+            			gui.messageArea.append(table[i][j] + " ");
+            		}
+            		gui.messageArea.append("\n");
+            	}*/
             } else if (line.startsWith("MESSAGE")) {
-                messageArea.append(line.substring(8) + "\n");
-            }
+                gui.messageArea.append(line.substring(8) + "\n");
+            } 
         }
+        
     }
 
 
     public static void main(String[] args) throws Exception {
         Client client = new Client();
-        client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        client.frame.setVisible(true);
+        client.gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        client.gui.setVisible(true);
         client.run();
     }
 }
--- src/network/Server.java
+++ src/network/Server.java
@@ -12,90 +12,142 @@ import java.util.HashSet;
 public class Server {
 
 
-    private static final int PORT = 9001;
+    private static final int PORT = 9006;
 
 
+    private static int currentPlayer = 1;
+    
+    public int getCurrentPlayer(){
+    	return currentPlayer;
+    }
+    
+    public void setCurrentPlayer(int c){
+    	currentPlayer = c;
+    }
+    
     private static HashSet<String> names = new HashSet<String>();
-
+    
     private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
-
 
     public static void main(String[] args) throws Exception {
         System.out.println("The chat server is running.");
         ServerSocket listener = new ServerSocket(PORT);
         try {
             while (true) {
-                new Handler(listener.accept()).start();
+                Player player1 = new Player(listener.accept(),1);
+                Player player2 = new Player(listener.accept(),2);
+                player1.setOpponent(player2);
+                player2.setOpponent(player1);
+                player1.start();
+                player2.start();
+                
             }
         } finally {
             listener.close();
         }
     }
+    
+    public static class Player extends Thread{
+    	private Integer id;
+    	private Socket socket;
+    	private BufferedReader in;
+    	private PrintWriter out;
+    	private String name;
+    	private Player opponent;
+    	
+    	private Integer[][] table;
+    	private int N;
+    	
+    	public Player(Socket socket,int id){
+    		this.socket = socket;
+    		this.id = id;
+    		N = 10;
+    		table = new Integer[N][N];
+    		for(int i = 0; i < N; ++i){
+    			for(int j = 0; j < N;++j){
+    				table[i][j] = 0;
+    			}
+    		}
+    	}
+    	
+    	public void setOpponent(Player opponent){
+    		this.opponent = opponent;
+    	}
+    	
+    	 public void run() {
+             try {
+                 in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 out = new PrintWriter(socket.getOutputStream(), true);
 
+                 while (true) {
+                     out.println("SUBMITNAME");
+                     name = in.readLine();
+                     if (name == null) {
+                         return;
+                     }
+                     synchronized (names) {
+                         if (!names.contains(name)) {
+                             names.add(name);
+                             break;
+                         }
+                     }
+                 }
 
-    private static class Handler extends Thread {
-        private String name;
-        private Socket socket;
-        private BufferedReader in;
-        private PrintWriter out;
-
-
-        public Handler(Socket socket) {
-            this.socket = socket;
-        }
-
-
-        public void run() {
-            try {
-
-                
-                in = new BufferedReader(new InputStreamReader(
-                    socket.getInputStream()));
-                out = new PrintWriter(socket.getOutputStream(), true);
-
-                while (true) {
-                    out.println("SUBMITNAME");
-                    name = in.readLine();
-                    if (name == null) {
-                        return;
-                    }
-                    synchronized (names) {
-                        if (!names.contains(name)) {
-                            names.add(name);
-                            break;
-                        }
-                    }
-                }
-
-        
-                out.println("NAMEACCEPTED");
-                writers.add(out);
-
-               
-                while (true) {
-                    String input = in.readLine();
-                    if (input == null) {
-                        return;
-                    }
-                    for (PrintWriter writer : writers) {
-                        writer.println("MESSAGE " + name + ": " + input);
-                    }
-                }
-            } catch (IOException e) {
-                System.out.println(e);
-            } finally {
          
-                if (name != null) {
-                    names.remove(name);
-                }
-                if (out != null) {
-                    writers.remove(out);
-                }
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                }
-            }
-        }
+                 out.println("NAMEACCEPTED");
+                 writers.add(out);
+                 out.println("SENDID" + id);
+                 
+                 out.println("INIT" + N);
+
+                 sendTable();
+                 
+                 
+                 while (true) {
+                     String input = in.readLine();
+                     if (input == null) {
+                         return;
+                     } else if(input.startsWith("CHAT")){
+                    	 for (PrintWriter writer : writers) {
+                             writer.println("MESSAGE " + name + ": " + input.substring(4));
+                         }
+                     } else if(input.startsWith("FIRE") && Server.currentPlayer == id){
+                    	 Integer i = Integer.parseInt(input.substring(5,6));
+	                     Integer j = Integer.parseInt(input.substring(7,8));
+	                     opponent.table[i][j] = 1;
+	                     Server.currentPlayer =  opponent.id;
+	                     opponent.sendTable();
+	                     sendTable();
+                     } 
+                     
+                 }
+                 
+             } catch (IOException e) {
+                 System.out.println(e);
+             } finally {
+          
+                 if (name != null) {
+                     names.remove(name);
+                 }
+                 if (out != null) {
+                     writers.remove(out);
+                 }
+                 try {
+                     socket.close();
+                 } catch (IOException e) {
+                 }
+             }
+         }
+    	 
+    	 
+    	 public void sendTable(){
+    		 for(int i = 0; i < N; ++i){
+            	 for(int j = 0; j < N; ++j){
+            		 out.println("FILL" + i + j + table[i][j]);
+            	 }
+             }
+            
+             out.println("TABLEDONE");
+    	 }
     }
 }
